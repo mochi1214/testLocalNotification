a9118dde01d2db665613ec2afebbab9305f99e49
吳佳臻 <tn00817411@gmail.com>
2019-05-16 14:28:45 +0800

    Initial Commit

---
+++ testLocalNotification/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testLocalNotification
+//
+//  Created by 金融研發一部-吳佳臻 on 2019/5/16.
+//  Copyright © 2019 金融研發一部-吳佳臻. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
