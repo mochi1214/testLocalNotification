34016ac9ca595edc163be5f0aef09c4376efa7c8
吳佳臻 <tn00817411@gmail.com>
2019-05-17 16:45:07 +0800

    本地推播

--- testLocalNotification/AppDelegate.swift
+++ testLocalNotification/AppDelegate.swift
@@ -7,15 +7,28 @@
 //
 
 import UIKit
+import UserNotifications
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate{
 
     var window: UIWindow?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        // 在程式一啟動即詢問使用者是否接受圖文(alert)、聲音(sound)、數字(badge)三種類型的通知
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert,.sound,.badge,.carPlay], completionHandler: {(granted, errno) in
+            if granted{
+                print("允許")
+            } else{
+                print("不允許")
+            }
+        })
+        
+        // 代理 UNUserNotificationCenterDelegate，這麼做可讓 App 在前景狀態下收到通知
+        UNUserNotificationCenter.current().delegate = self
+        
         return true
     }
 
@@ -39,6 +52,23 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+    
+    // 在前景收到通知時所觸發的 function
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        print("在前景收到通知...")
+        completionHandler([.badge, .sound, .alert])
+    }
+    
+    // 點擊通知觸發的事件
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        let content: UNNotificationContent = response.notification.request.content
+        
+        completionHandler()
+        
+        // 取出userInfo的link並開啟Facebook
+        let requestUrl = URL(string: content.userInfo["link"]! as! String)
+        UIApplication.shared.open(requestUrl!, options: [:], completionHandler: nil)
     }
 
 
--- testLocalNotification/ViewController.swift
+++ testLocalNotification/ViewController.swift
@@ -7,13 +7,53 @@
 //
 
 import UIKit
+import UserNotifications
+
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // 點擊建立通知按鈕
+        let createNotificationBtn: UIButton = UIButton()
+        createNotificationBtn.frame = CGRect(x: self.view.frame.size.width / 2 - 100, y: self.view.frame.size.height / 2 - 25, width: 200, height: 50)
+        createNotificationBtn.titleLabel?.font = UIFont.systemFont(ofSize: 30)
+        createNotificationBtn.setTitle("按下推播通知", for: .normal)
+        createNotificationBtn.setTitleColor(UIColor.white, for: .normal)
+        createNotificationBtn.layer.cornerRadius = 10
+        createNotificationBtn.backgroundColor = UIColor.darkGray
+        createNotificationBtn.addTarget(self, action: #selector(ViewController.onClickCreateNotificationBtn(_:)), for: UIControl.Event.touchUpInside)
+        self.view.addSubview(createNotificationBtn)
     }
+    
+    
+    @objc func onClickCreateNotificationBtn(_ sender: UIButton) {
+        let content = UNMutableNotificationContent()
+        content.title = "測試本地通知"
+        content.subtitle = "本地推播練習"
+        content.body = "推播成功"
+        content.badge = 1
+        content.sound = UNNotificationSound.default
+        
+        //設置推播通知圖片
+        let imageURL: URL = Bundle.main.url(forResource: "capoo", withExtension: "jpg")!
+        let attachment = try! UNNotificationAttachment(identifier: "image", url: imageURL, options: nil)
+        content.attachments = [attachment]
+        
+        //點擊推播通知後取得的資訊
+        content.userInfo = ["link" : "https://www.facebook.com/chiachen.wu.319"]
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        
+        let request = UNNotificationRequest(identifier: "notification", content: content, trigger: trigger)
+        
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: {error in
+            print("成功建立通知...")
+        })
+    }
+    
+   
 
 
 }
